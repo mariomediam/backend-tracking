5be58ff291d33e561e43c79e0277a4c8bb30b5a1
mariomediam <54757091+mariomediam@users.noreply.github.com>
2021-10-29 08:17:03 -0500

    chore: set path pdfkit

--- controllers/to_pdf.py
+++ controllers/to_pdf.py
@@ -1,14 +1,26 @@
 from flask import make_response, render_template
 import pdfkit
+import os
 
 def pdf_template(**kwargs):
     rendered = render_template(kwargs["template"], **kwargs)
-    config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
-    #pdf = pdfkit.from_string(rendered, "/app/static/pdfs/{}.pdf".format(kwargs["output"]), configuration=config)
-    #pdf = pdfkit.from_string("rendered", "/app/tmp/{}.pdf".format(kwargs["output"]), configuration=config)
+
+    #LOCAL
     #pdf = pdfkit.from_string(rendered, "./static/pdfs/{}.pdf".format(kwargs["output"]))
-    pdf = pdfkit.from_string("Holaaaa", "/app/tmp/output.pdf", verbose=True, configuration=config)
-    #pdfkit.from_string('Hello!', 'out.pdf')
-    return pdf
+
+    #HEROKU
+
+    #config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+    #pdf = pdfkit.from_string(rendered, "/app/static/pdfs/{}.pdf".format(kwargs["output"]), configuration=config)
+    
+    #pdf = pdfkit.from_string("rendered", "/app/tmp/{}.pdf".format(kwargs["output"]), configuration=config)
+    
+    #pdf = pdfkit.from_string("Holaaaa", "/app/tmp/output.pdf", configuration=config )
+
+    file = open("/app/tmp/filename.txt", "w")
+    file.write("Primera línea" + os.linesep)
+    file.write("Segunda línea")
+    file.close()
+    return file
     
 
