9ec4e05d9092bd6fc8148b9f13b1c26b188706d1
mariomediam <54757091+mariomediam@users.noreply.github.com>
2021-10-29 00:14:03 -0500

    chore: set path pdfkit

--- controllers/to_pdf.py
+++ controllers/to_pdf.py
@@ -5,7 +5,9 @@ def pdf_template(**kwargs):
     rendered = render_template(kwargs["template"], **kwargs)
     config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
     #pdf = pdfkit.from_string(rendered, "/app/static/pdfs/{}.pdf".format(kwargs["output"]), configuration=config)
-    pdf = pdfkit.from_string(rendered, "/app/tmp/{}.pdf".format(kwargs["output"]), configuration=config)
+    #pdf = pdfkit.from_string("rendered", "/app/tmp/{}.pdf".format(kwargs["output"]), configuration=config)
     #pdf = pdfkit.from_string(rendered, "./static/pdfs/{}.pdf".format(kwargs["output"]))
+    pdf = pdfkit.from_string("Holaaaa", "/app/tmp/output.pdf", configuration=config)
+    #pdfkit.from_string('Hello!', 'out.pdf')
     
 
