cb7b14f2855e60bebed51bfccf449d9bf7280f3e
mariomediam <54757091+mariomediam@users.noreply.github.com>
2021-10-28 21:41:43 -0500

    chore: set pdfkit

--- controllers/to_pdf.py
+++ controllers/to_pdf.py
@@ -3,5 +3,6 @@ import pdfkit
 
 def pdf_template(**kwargs):
     rendered = render_template(kwargs["template"], **kwargs)
-    pdf = pdfkit.from_string(rendered, "./static/pdfs/{}.pdf".format(kwargs["output"]))
+    config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+    pdf = pdfkit.from_string(rendered, "./static/pdfs/{}.pdf".format(kwargs["output"]), configuration=config)
 
